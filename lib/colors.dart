3cc97683610c6bcdb4ebc34c20b70579d7a9a67a
GrantR <grantrajapakse@gmail.com>
2022-07-07 12:32:07 -0700

    colors updated

--- lib/colors.dart
+++ lib/colors.dart
@@ -22,24 +22,29 @@ Color brightness(Color color, double shift) {
 ///                     Branded Stat Colors                   ///
 ///                                                           ///
 /////////////////////////////////////////////////////////////////
-Color cpuColor = const Color.fromARGB(255, 40, 197, 19);
-Color memColor = const Color.fromARGB(255, 255, 200, 0);
-Color netColor = const Color.fromARGB(255, 4, 98, 205);
-Color gpuColor = const Color.fromARGB(255, 218, 5, 76);
+Color cpuColor = Color.fromARGB(255, 124, 201, 113);
+Color memColor = Color.fromARGB(255, 241, 203, 99);
+Color netColor = Color.fromARGB(255, 94, 225, 248);
+Color gpuColor = Color.fromARGB(255, 242, 84, 247);
 
 ////
 // Color sixty = Color.fromARGB(255, 247, 247, 247);
 // Color thirty = Color.fromARGB(255, 188, 26, 150);
 // Color thirtyAccent = const Color.fromARGB(255, 0, 165, 148);
 
-
 /////////////////////////////////////////////////////////////////
 ///                                                           ///
 ///         THESE ARE THE ONLY VALUES THAT GET CHANGED        ///
 ///                                                           ///
 /////////////////////////////////////////////////////////////////
-Color sixty = const Color.fromARGB(255, 31, 42, 47);
-Color thirty = const Color.fromARGB(255, 89, 113, 254);
+Color sixty =
+//Color.fromARGB(255, 19, 155, 1);
+    Color.fromARGB(255, 255, 255, 255);
+// const Color.fromARGB(255, 31, 42, 47);
+Color thirty =
+//Color.fromARGB(255, 255, 0, 234);
+    Color.fromARGB(255, 2, 19, 116);
+// const Color.fromARGB(255, 89, 113, 254);
 Color ten = Colors.pinkAccent;
 
 ///Color Map:
@@ -53,10 +58,9 @@ Color ten = Colors.pinkAccent;
 //  Layer 1 Accent
 //     Layer 1
 
-Color zeroBaseColor = brightness(sixty, 0.75);
+Color zeroBaseColor = brightness(sixty, 0.8);
 Color zeroLayerColor = brightness(thirty, 0.6);
 Color zeroLayerAccentColor = brightness(thirty, 0.75);
 Color oneBaseColor = sixty;
 Color oneLayerColor = brightness(thirty, 0.8);
-Color oneLayerAccentColor = brightness(thirty, 1);//thirtyAccent;
-
+Color oneLayerAccentColor = brightness(thirty, 1); //thirtyAccent;
